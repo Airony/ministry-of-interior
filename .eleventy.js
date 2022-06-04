6158c4bece9e712cd2eea79c01917e6b2ff25fb0
Airony <dxangusx@gmail.com>
2022-06-04 11:55:24 +0100

    refac: Put Wordpress API link in an env file for security/ease of change.

--- .eleventy.js
+++ .eleventy.js
@@ -1,34 +1,39 @@
+require("dotenv").config();
+
 module.exports = (config) => {
-  //   config.addWatchTarget("./content/assets/");
-  config.setBrowserSyncConfig({
-    files: "./build/css/**/*.css",
-  });
-  config.addPassthroughCopy("content/assets");
-  config.addPassthroughCopy("content/scripts");
+    //   config.addWatchTarget("./content/assets/");
+    config.setBrowserSyncConfig({
+        files: "./build/css/**/*.css",
+    });
+    config.addPassthroughCopy("content/assets");
+    config.addPassthroughCopy("content/scripts");
 
-  config.addFilter("myfilter", (headings) => {
-    return constructList(headings, 1);
-  });
+    config.addFilter("myfilter", (headings) => {
+        return constructList(headings, 1);
+    });
 
-  return {
-    dir: {
-      input: "content",
-      output: `build`,
-    },
-  };
+    return {
+        dir: {
+            input: "content",
+            output: `build`,
+        },
+    };
 };
 
 function constructList(headings, currentDepth) {
-  let result = "";
-  for (let i = 0; i < headings.length; i++) {
-    const heading = headings[i];
+    let result = "";
+    for (let i = 0; i < headings.length; i++) {
+        const heading = headings[i];
 
-    result += `<li class='toc__level-${currentDepth}'>
+        result += `<li class='toc__level-${currentDepth}'>
                   <a href='#${heading.id}' class='toc__item'>${heading.content}</a>`;
-    if (heading.children) {
-      result += `<ul>${constructList(heading.children, currentDepth + 1)}</ul>`;
+        if (heading.children) {
+            result += `<ul>${constructList(
+                heading.children,
+                currentDepth + 1
+            )}</ul>`;
+        }
+        result += `</li>`;
     }
-    result += `</li>`;
-  }
-  return result;
+    return result;
 }
