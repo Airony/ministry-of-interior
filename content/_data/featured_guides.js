4e966f8a366422dfdde07a26761be2085bd8f55d
Airony <dxangusx@gmail.com>
2022-06-04 12:55:46 +0100

    feat: Fetch featured guides from Wordpress API instead of using a placeholder file style changes.
    This change also brings style changes to the guides section because of some overlooked problems that became apparent (text-overflow,text onimage contrast problems)

---
+++ content/_data/featured_guides.js
@@ -0,0 +1,17 @@
+const fetch = require("node-fetch");
+const parser = require("node-html-parser");
+
+const FEATURED_GUIDES_API_LINK =
+    process.env["WORDPRESS_HOST"] + "wp-json/homepage-settings/featured-guides";
+
+module.exports = async function () {
+    const res = await fetch(`${FEATURED_GUIDES_API_LINK}`),
+        json = await res.json();
+    return json.map((guide) => {
+        return {
+            title: guide.post_title,
+            content: guide.no_tag_content.substring(0, 80).trim() + "...",
+            image: guide.thumbnail,
+        };
+    });
+};
