32276233261fb9dd742506d80ce72bc2b3291411
Airony <dxangusx@gmail.com>
2022-06-05 22:38:07 +0100

    fix: Catch error when fetching featured guides from API.

--- content/_data/featured_guides.js
+++ content/_data/featured_guides.js
@@ -4,14 +4,19 @@ const FEATURED_GUIDES_API_LINK =
     process.env["WORDPRESS_HOST"] + "wp-json/homepage-settings/featured-guides";
 
 module.exports = async function () {
-    const res = await fetch(`${FEATURED_GUIDES_API_LINK}`),
-        json = await res.json();
-    return json.map((guide) => {
-        return {
-            title: guide.post_title,
-            content: guide.no_tag_content.substring(0, 80).trim() + "...",
-            image: guide.thumbnail,
-            link: helper.cleanWpLink(guide.link),
-        };
-    });
+    try {
+        const res = await fetch(`${FEATURED_GUIDES_API_LINK}`),
+            json = await res.json();
+        return json.map((guide) => {
+            return {
+                title: guide.post_title,
+                content: guide.no_tag_content.substring(0, 80).trim() + "...",
+                image: guide.thumbnail,
+                link: helper.cleanWpLink(guide.link),
+            };
+        });
+    } catch (err) {
+        console.log("Fetching featured guides failed: " + err);
+        return 0;
+    }
 };
