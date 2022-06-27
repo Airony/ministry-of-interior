734debbd5d77d6eb80abd562f58d942613ebaa45
Airony <dxangusx@gmail.com>
2022-06-27 13:29:40 +0100

    fix: Fix scroll staying stuck when opening the nav menu and resizing the screen.
    This bug is caused when a modal that locks the scroll is opened at lower screen sizes, and then the user resizes the screen causing the modal to disappear without unlocking the scroll first.
    Fixed it by adding a --collapsed attribute to the modal, and adding a resize observer for the modal that checks the attribute and unstucks the scroll when it is no longer collapsed

--- content/scripts/modal-control.js
+++ content/scripts/modal-control.js
@@ -1,41 +1,71 @@
 window.addEventListener("load", function () {
-  const modals = this.document.querySelectorAll("[data-modal='true']");
+    const modals = this.document.querySelectorAll("[data-modal='true']");
 
-  modals.forEach((modal) => {
-    bindModalButtons(modal);
-  });
+    modals.forEach((modal) => {
+        const modalControls = document.querySelectorAll(
+            `button[data-modal-btn="true"][aria-controls="${modal.id}"]`
+        );
+        console.log(modalControls);
+        bindModalButtons(modal, modalControls);
+        if (modal.dataset.preventScroll == "true") {
+            observeModalCollapseState(modal, modalControls);
+        }
+    });
 });
 
-function bindModalButtons(modal) {
-  const buttons = document.querySelectorAll(
-    `button[data-modal-btn="true"][aria-controls="${modal.id}"]`
-  );
-  buttons.forEach((button) => {
-    console.log(button);
-    button.addEventListener("click", () => {
-      toggleModal(modal, buttons);
+function bindModalButtons(modal, controls) {
+    controls.forEach((button) => {
+        button.addEventListener("click", () => {
+            toggleModal(modal, controls);
+        });
     });
-  });
 }
 
-function toggleModal(modal, buttons) {
-  const expanded =
-    buttons[0].getAttribute("aria-expanded") == "true" ? true : false;
-  if (expanded) {
-    buttons.forEach((button) => button.setAttribute("aria-expanded", "false"));
+function toggleModal(modal, controls) {
+    const expanded =
+        controls[0].getAttribute("aria-expanded") == "true" ? true : false;
+    if (expanded) {
+        closeModal(modal, controls);
+    } else {
+        openModal(modal, controls);
+    }
+}
+
+function closeModal(modal, controls) {
+    controls.forEach((button) => button.setAttribute("aria-expanded", "false"));
     modal.classList.remove("menu--expanded");
     if (modal.getAttribute("data-prevent-scroll") == "true") {
-      const scrollY = document.body.style.top;
-      document.body.style.position = "";
-      document.body.style.top = "";
-      window.scrollTo(0, parseInt(scrollY || "0") * -1);
+        unlockScroll();
     }
-  } else {
-    buttons.forEach((button) => button.setAttribute("aria-expanded", "true"));
+}
+
+function openModal(modal, controls) {
+    controls.forEach((button) => button.setAttribute("aria-expanded", "true"));
     modal.classList.add("menu--expanded");
     if (modal.getAttribute("data-prevent-scroll") == "true") {
-      document.body.style.top = `-${window.scrollY}px`;
-      document.body.style.position = "fixed";
+        lockScroll();
     }
-  }
+}
+
+function lockScroll() {
+    document.body.style.top = `-${window.scrollY}px`;
+    document.body.style.position = "fixed";
+}
+
+function unlockScroll() {
+    const scrollY = document.body.style.top;
+    document.body.style.position = "";
+    document.body.style.top = "";
+    window.scrollTo(0, parseInt(scrollY || "0") * -1);
+}
+
+function observeModalCollapseState(modal, controls) {
+    const resizeObserver = new ResizeObserver(() => {
+        const collapsed =
+            getComputedStyle(modal).getPropertyValue("--collapsed");
+        if (collapsed !== true && modal.classList.contains("menu--expanded")) {
+            closeModal(modal, controls);
+        }
+    });
+    resizeObserver.observe(modal);
 }
